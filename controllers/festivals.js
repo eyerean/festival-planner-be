7bd152957a8700dc7f6bd4cb369610324647d073
eyerean <eptinea@gmail.com>
2018-06-10 20:43:40 +0200

    rename name property of festival schema to festivalName

--- controllers/festivals.js
+++ controllers/festivals.js
@@ -1,24 +1,24 @@
 const Festival = require('../models/festival');
 
 exports.create = (req, res, next) => {
-  const name = req.body.name;
+  const festivalName = req.body.festivalName;
   const startDate = req.body.startDate;
   const endDate = req.body.endDate;
   const status = req.body.status;
   const desc = req.body.desc;
 
-  if(!name){
+  if(!festivalName){
     return res.status(422).send({ error: 'You must provide a name for the festival'});
   }
 
-  Festival.findOne({ name: name}, (err, existingFestival) => {
+  Festival.findOne({ festivalName: festivalName}, (err, existingFestival) => {
     if (err) throw err;
     if (existingFestival) {
       return res.status(422).send({error: 'Festival already exists'});
     }
 
     const festival = new Festival({
-      name: name,
+      festivalName: festivalName,
       startDate: startDate,
       endDate: endDate,
       description: desc,
--- models/festival.js
+++ models/festival.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const festivalSchema = new Schema({
-  name: String,
-  startDate: String, // (ISOString)
-  endDate: String, // (ISOString)
+  festivalName: String,
+  startDate: String, // (ISOString?)
+  endDate: String, // (ISOString?)
   description: String,
   status: String
 });
